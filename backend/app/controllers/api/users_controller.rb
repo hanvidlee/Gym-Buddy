6e33ceb11f6e941b43912077e69e10d077585a08
Wendina <wendina.teng@gmail.com>
2023-05-13 19:22:42 +0000

    remove seeds info from user controller. add  sususer seeds to seed filke, add new gem 'faker'

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,14 +1,7 @@
-require('faker')
-
 class Api::UsersController < ApplicationController
   # index, create, new, update, edit, destroy
   def index
     users = User.all
     render json: users
-  end
-
-
-  3.times do
-    User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password)
   end
 end
