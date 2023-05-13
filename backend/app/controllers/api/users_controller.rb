fe989ecfc0d6991dae08d79405d211d675477750
Wendina <wendina.teng@gmail.com>
2023-05-13 17:56:50 +0000

    set up route for users in routes.rb and users_controller.rb

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -2,6 +2,12 @@ require('faker')
 
 class Api::UsersController < ApplicationController
   # index, create, new, update, edit, destroy
+  def index
+    users = User.all
+    render json: users
+  end
+
+
   3.times do
     User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password)
   end
