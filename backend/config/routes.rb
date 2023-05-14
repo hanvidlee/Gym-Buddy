7b46f8cd7bc50a0cdd590ed49697f21e4c65b207
Wendina <wendina.teng@gmail.com>
2023-05-14 18:11:29 +0000

    generate controller for exercises and create route for it

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'exercises/index'
+  get 'exercises/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :users
@@ -7,5 +9,7 @@ Rails.application.routes.draw do
     resources :workouts
     
     resources :user_workouts
+
+    resources :exercises, only: [:index, :show]
   end
 end
