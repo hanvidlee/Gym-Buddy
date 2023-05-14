54bfdc34120f1239b7d5e911ec3fdf63966efd1f
Wendina <wendina.teng@gmail.com>
2023-05-14 18:08:00 +0000

    add post login route to users conts controller. add logic to users controller file.

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -4,4 +4,15 @@ class Api::UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+  def login
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: { success: true, user: user }
+    else
+      render json: { success: false, message: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
 end
