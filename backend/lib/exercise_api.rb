960d40cac45d092ffe0f337bbb6aa915fd07b9a5
Wendina <wendina.teng@gmail.com>
2023-05-15 01:47:32 +0000

    add new file for exercise api and add logic to it using net/http to request the info

---
+++ backend/lib/exercise_api.rb
@@ -0,0 +1,23 @@
+require 'net/http'
+
+class ExerciseAPI
+  API_BASE_URL = 'https://api.api-ninjas.com/v1'
+  API_KEY = ENV['API_KEY']
+
+  def self.get_exercises_by_muscle(muscle)
+    api_url = "#{API_BASE_URL}/exercises?muscle=#{muscle}"
+    uri = URI(api_url)
+    request = Net::HTTP::Get.new(uri)
+    request['X-Api-Key'] = API_KEY
+
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    if response.code == '200'
+      JSON.parse(response.body)
+    else
+      raise "Error: #{response.code} #{response.body}"
+    end
+  end
+end
