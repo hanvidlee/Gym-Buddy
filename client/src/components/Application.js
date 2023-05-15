4314fd417ea3858e7906dacdaf3a4a16b2b2e046
Wendina <wendina.teng@gmail.com>
2023-05-15 01:24:28 +0000

    create new file for drop down menu, add logic and call it in Application.js

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,26 +1,34 @@
 import '../App.css';
 import useApplicationData from '../hooks/useApplicationData';
-import Button from '../Button'
+import Button from '../Button';
+import DropdownMenu from './DropDownMenu';
 
 function Application(props) {
   const checkConfirm = () => {
-    console.log("CONFIRMED!")
-  }
+    console.log('CONFIRMED!');
+  };
   const checkDanger = () => {
-    console.log("DANGER!!")
-  }
-  const {
-    state,
-    dispatch
-} = useApplicationData();
-  const userList = state.users.map((user) => (<li key={user.id} > {user.name} {user.email} </li>
-));
-return (<div className="App" >
-<h1> Users </h1>
-<Button onClick={checkConfirm} confirm>Confirm</Button>
-<Button onClick={checkDanger} danger>Danger</Button>
-<ul> {userList} </ul>
-</div >
-);
+    console.log('DANGER!!');
+  };
+  const { state, dispatch } = useApplicationData();
+  const userList = state.users.map((user) => (
+    <li key={user.id}>
+      {' '}
+      {user.name} {user.email}{' '}
+    </li>
+  ));
+  return (
+    <div className="App">
+      <h1> Users </h1>
+      <Button onClick={checkConfirm} confirm>
+        Confirm
+      </Button>
+      <Button onClick={checkDanger} danger>
+        Danger
+      </Button>
+      <ul> {userList} </ul>
+      <DropdownMenu>OPTIONS</DropdownMenu>
+    </div>
+  );
 }
 export default Application;
