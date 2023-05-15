0648c632e7fd9007c02f9b4c92260d5965f8e7c8
nelsonchengg <nelsoncheng103@gmail.com>
2023-05-15 15:40:07 -0400

    merge conflict fix

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -2,11 +2,14 @@ import '../App.css';
 import useApplicationData from '../hooks/useApplicationData';
 import Button from './Button';
 import DropdownMenu from './DropDownMenu';
+import Button from './Button';
 import React, { useReducer } from 'react';
 
 function Application(props) {
   const checkConfirm = () => {
     console.log('CONFIRMED!');
+  };
+    console.log("CONFIRMED!");
   };
   const checkDanger = () => {
     console.log('DANGER!!');
@@ -31,6 +34,22 @@ function Application(props) {
       <ul> {userList} </ul>
       <DropdownMenu>OPTIONS</DropdownMenu>
     </div>
+  );
+    console.log("DANGER!!");
+  };
+  const {
+    state,
+    dispatch
+  } = useApplicationData();
+  const userList = state.users.map((user) => (<li key={user.id} > {user.name} {user.email} </li>
+  ));
+  return (<div className="App" >
+    <h1> Users </h1>
+    <Button
+      onClick={checkConfirm} confirm>Confirm</Button>
+    <Button onClick={checkDanger} danger>Danger</Button>
+    <ul> {userList} </ul>
+  </div >
   );
 }
 export default Application;
