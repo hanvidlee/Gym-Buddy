5efbaf2883acf5e585bae90ec3aaa5d590c82b90
Wendina <wendina.teng@gmail.com>
2023-05-15 18:56:05 +0000

    modify the component to sue custom drop down menu react rather than the built in select and option

--- client/src/components/DropDownMenu.js
+++ client/src/components/DropDownMenu.js
@@ -3,25 +3,41 @@ import React, { useState, useEffect } from 'react';
 const DropdownMenu = () => {
   const [exercises, setExercises] = useState([]);
 
+  // fetch exercise api from backend
   useEffect(() => {
     fetch('/api/exercises')
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         setExercises(data);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
 
   return (
-    <select>
-      {exercises.map(exercise => (
-        <option key={exercise.id} value={exercise.name}>
-          {exercise.name}
-        </option>
-      ))}
-    </select>
+    <div className="menu-container">
+      <div className="menu-trigger" >
+        <p>Select Exercises</p>
+      </div>
+
+      <div className='dropdown-menu'>
+        <h3>Filter</h3>
+        <ul>
+          {exercises.map((exercise) => (
+            <DropdownItem key={exercise.id} text={exercise.name} />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
+
+function DropdownItem(props) {
+  return (
+    <li className="dropdownItem">
+      <p>{props.text}</p>
+    </li>
+  );
+}
 
 export default DropdownMenu;
