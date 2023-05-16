f69ad3be465c6d0acec72c098b14c192ede411b6
Wendina <wendina.teng@gmail.com>
2023-05-16 17:32:21 -0400

    add some css to make everything set in place

--- client/src/components/Calendar/Calender.js
+++ client/src/components/Calendar/Calender.js
@@ -7,19 +7,22 @@ export default function Calendar() {
   const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
   return (
-    <div className='w-96 h-96'>
-      <div className='w-full grid grid-cols-7'>
+    <div className="w-96 h-96">
+      <div className="w-full grid grid-cols-7">
         {days.map((day, index) => {
           return (
-            <h1 key={index}>{day}</h1>
-          )
+            <h1 key={index} 
+            className="h-14 border-b grid place-content-center text-sm">
+              {day}
+            </h1>
+          );
         })}
       </div>
 
-      <div className='w-full grid grid-cols-7'>
+      <div className="w-full grid grid-cols-7">
         {generateDate().map(({ date, currentMonth, today }, index) => {
           return (
-            <div key={index}>
+            <div key={index} className="h-14 grid place-content-center">
               <h1>{date.date()}</h1>
             </div>
           );
