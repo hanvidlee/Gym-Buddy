87d89b092cced1d91e0af41fc6a4e29b4fb9d352
nelsonchengg <nelsoncheng103@gmail.com>
2023-05-16 00:17:30 -0400

    create password_digest

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_15_185946) do
+ActiveRecord::Schema.define(version: 2023_05_16_041422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,10 +71,10 @@ ActiveRecord::Schema.define(version: 2023_05_15_185946) do
     t.string "name"
     t.string "username"
     t.string "email"
-    t.string "password"
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "workouts", force: :cascade do |t|
