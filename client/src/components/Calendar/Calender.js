dbaece62655cf99115d60c20d058da5e07764e16
Wendina <wendina.teng@gmail.com>
2023-05-16 17:16:27 -0400

    call generateDate function andd map over the returned values and apply tailwind css to generate the ui

--- client/src/components/Calendar/Calender.js
+++ client/src/components/Calendar/Calender.js
@@ -6,8 +6,15 @@ export default function Calendar() {
   console.log(generateDate());
 
   return(
-    <div>
-      Calendar
+    <div className="w-96 h-96 grid grid-cols-7">
+
+    {generateDate().map(({ date, currentMonth, today }, index) => {
+      return (
+        <div>
+          <h1>{date.date()}</h1>
+        </div>
+      );
+    })}
     </div>
   )
 }
