aa867a715836059d2fa98f4f103c692e69384b57
hanvidlee <hanvidlee@gmail.com>
2023-05-16 05:58:28 -0400

    add json render methods for reps controller

--- backend/app/controllers/api/reps_controller.rb
+++ backend/app/controllers/api/reps_controller.rb
@@ -1,19 +1,51 @@
-class RepsController < ApplicationController
+class Api::RepsController < ApplicationController
   def index
+    @reps = Rep.all
+    render json: @reps
+  end
+
+  def show
+    @rep = Rep.find(params[:id])
+    render json: @rep
   end
 
   def new
+    @rep = Rep.new
   end
 
   def create
-  end
+    @rep = Rep.new(rep_params)
 
-  def update
+      if @rep.save
+        render json: @rep
+      else
+        render json: @rep.errors
   end
 
   def edit
+    @rep = Rep.find(params[:id])
+    render json: @rep
   end
 
-  def delete
+  def update
+    @rep = Rep.find(params[:id])
+
+    if @rep.update(rep_params)
+      render json: @rep
+    else
+      render json: @rep.errors #check what error comes out/how to access
+    end
+  end
+
+  def destroy
+    @rep = Rep.find(params[:id])
+    @rep.destroy
+
+  end
+
+  private
+
+  def rep_params
+    params.require(:rep).permit(:quantity)
   end
 end
