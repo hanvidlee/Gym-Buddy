97dbaae704e0e9771a27e27c88ba47f91af5dc07
Wendina <wendina.teng@gmail.com>
2023-05-16 17:21:58 -0400

    create days and map over them to return each day and add tailwind css. add key to each day and date.

--- client/src/components/Calendar/Calender.js
+++ client/src/components/Calendar/Calender.js
@@ -1,20 +1,30 @@
-import React from "react";
-import { generateDate } from "./calendarUtil";
+import React from 'react';
+import { generateDate } from './calendarUtil';
 
 export default function Calendar() {
-
   console.log(generateDate());
 
-  return(
-    <div className="w-96 h-96 grid grid-cols-7">
+  const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
-    {generateDate().map(({ date, currentMonth, today }, index) => {
-      return (
-        <div>
-          <h1>{date.date()}</h1>
-        </div>
-      );
-    })}
+  return (
+    <div className='w-96 h-96'>
+      <div className='w-full grid grid-cols-7'>
+        {days.map((day, index) => {
+          return (
+            <h1 key={index}>{day}</h1>
+          )
+        })}
+      </div>
+
+      <div className='w-full grid grid-cols-7'>
+        {generateDate().map(({ date, currentMonth, today }, index) => {
+          return (
+            <div key={index}>
+              <h1>{date.date()}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
