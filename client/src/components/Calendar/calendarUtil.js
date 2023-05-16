907cfcdcf87aaa46a474f566cc5b581de7c3940e
Wendina <wendina.teng@gmail.com>
2023-05-16 13:28:20 -0400

    create function generrateDate in calendarUtil.js file. It takes in paramaters of month and year.

---
+++ client/src/components/Calendar/calendarUtil.js
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs';
+
+export const generateDate = (
+  // default value of these are current month and year, they are indexed
+  month = dayjs().month,
+  year = dayjs().year
+  ) => {
+
+    
+};
