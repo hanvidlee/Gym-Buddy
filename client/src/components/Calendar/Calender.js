7b5755e25f70ca1b0ac7c31993811c597302f8d7
Wendina <wendina.teng@gmail.com>
2023-05-16 20:03:03 -0400

    add onclick handler to the <GrFormNext> and 'Today' as well

--- client/src/components/Calendar/Calender.js
+++ client/src/components/Calendar/Calender.js
@@ -28,8 +28,16 @@ export default function Calendar() {
             className="w-5 h-5 cursor-pointer" 
             onClick={() => {setToday(today.month(today.month() - 1))}}
             /> 
-            <h1 className="cursor-pointer">Today</h1>
-            <GrFormNext className="w-5 h-5 cursor-pointer" />
+            <h1 
+            className="cursor-pointer"
+            onClick={() => {setToday(currentDate)}}
+            >
+              Today
+            </h1>
+            <GrFormNext 
+            className="w-5 h-5 cursor-pointer" 
+            onClick={() => {setToday(today.month(today.month() + 1))}}
+            />
           </div>
         </div>
 
