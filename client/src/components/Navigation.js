9eef465f5e921b555477a41b406bb369f7427661
hanvidlee <hanvidlee@gmail.com>
2023-05-17 05:38:49 -0400

    minor tweaks to routing, add basic css and links for navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Navigation.scss';
 
 export default function Navigation(props) {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Gym Buddy</Navbar.Brand>
-        <Nav className="me-auto">
-          <Link to={'/'}>Home</Link>
-          <Link to={'/history'}>History</Link>
-          <Link to={'/calendar'}> Calendar</Link>
-        </Nav>
-        <Link to={'/login'}><Button variant="outline-success">Logout</Button></Link>
-      </Container>
-    </Navbar>
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-left">
+          <Link to={'/'}><div className="navbar-brand text-white">Gym Buddy</div></Link>
+        </div>
+        <div className="navbar-center">
+          <Link to={'/'} className="navbar-link">Home</Link>
+          <Link to={'/history'} className="navbar-link">History</Link>
+          <Link to={'/calendar'} className="navbar-link">Calendar</Link>
+        </div>
+        <div className="navbar-right">
+          <div className="navbar-welcome">
+            <span>Welcome!</span>
+          </div>
+          <Link to={'/login'} className="btn btn-outline-light">Logout</Link>
+        </div>
+      </div>
+    </nav>
   );
 }
