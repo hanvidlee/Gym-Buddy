9c17f09a260c9343146dc0670b1c6759438c80a2
Wendina <wendina.teng@gmail.com>
2023-05-19 15:41:11 -0400

    help with set up

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 const Home = () => {
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/workouts').then((res) => {
+      console.log('res', res);
+    });
+  }, []);
   return (
     <div>
       <h1>DASHBOARD</h1>
-    {/* render  */}
+      {/* render  */}
     </div>
   );
 };
