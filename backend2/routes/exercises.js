01c38499417ae22beba62cbb98ae49afad4a40c5
Wendina <wendina.teng@gmail.com>
2023-05-20 12:01:13 -0400

    create api request

--- backend2/routes/exercises.js
+++ backend2/routes/exercises.js
@@ -1,17 +1,46 @@
-// THIS IS NOT NEEDED
-
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
+const request = require('request');
 const { getAllDetailsPerExercise } = require('../db/queries/exercises');
 
-router.get('/', (req, res) => {
-  const user_id = req.session.userId;
-  const exercise = req.body.exercise;
+// router.get('/', async (req, res) => {
 
-  getAllDetailsPerExercise(1, "Push Ups").then((data) => {
-    console.log('data', data);
-    return res.send(data);
-  });
+//   // getAllDetailsPerExercise(1, "Push Ups").then((data) => {
+//   //   console.log('data', data);
+//   //   return res.send(data);
+//   // });
+// });
+const apiKey = "786ef654bemsh89a3687e7153b6fp13579ejsn98b50a66d1f2";
+
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://exercisedb.p.rapidapi.com/exercises', {
+      headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 module.exports = router;
+
+// url = URI("https://exercisedb.p.rapidapi.com/exercises")
+
+// http = Net::HTTP.new(url.host, url.port)
+// http.use_ssl = true
+
+// request = Net::HTTP::Get.new(url)
+
+// request["X-RapidAPI-Key"] = Rails.application.config.api_key
+
+// request["X-RapidAPI-Host"] = 'exercisedb.p.rapidapi.com'
+
+// response = http.request(request)
+// render json: response.read_body
