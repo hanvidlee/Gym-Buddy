89f8c0221e563e8164df4e6e5899e36e50a306e0
Wendina <wendina.teng@gmail.com>
2023-05-20 08:30:09 -0400

    add routes skeleton

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -31,6 +31,78 @@ export default function useApplicationData() {
     }).catch((error) => {
       console.log("This is the error from connection: ", error)
     })
+
+    axios.post('http://localhost:8080/api/sets', {
+      workout_id: ,// how to update correctly
+      weight: , 
+      reps: ,
+      quantity: ,
+      exercise: 
+    }).then((response) => {
+      console.log('Sets added successfully');
+      // setState()
+    }).catch((error) => {
+      console.log('There is an error adding sets: ', error);
+    })
+
+    axios.post('http://localhost:8080/api/sets/edit', {
+      set_id: ,// how to update correctly
+      weight: , 
+      reps: ,
+      quantity: ,
+      exercise: 
+    }).then((response) => {
+      console.log('Sets updated successfully');
+      // setState()
+    }).catch((error) => {
+      console.log('There is an error updating sets: ', error);
+    })
+
+    axios.post('http://localhost:8080/api/sets/remove', {
+      set_id: 
+    }).then((response) => {
+      console.log('Set removed successfully');
+      // setState()
+    }).catch((error) => {
+      console.log('There is an error removing set ', error);
+    });
+
+    axios.post('http://localhost:8080/api/workouts', {
+      user_id: ,// how to update correctly
+      day_id: , 
+      picture_url: ,
+      description: ,
+      title: 
+    }).then((response) => {
+      console.log('Workouts added successfully');
+      // setState()
+    }).catch((error) => {
+      console.log('There is an error adding workouts: ', error);
+    })
+
+    axios.post('http://localhost:8080/api/workouts/edit', {
+      workout_id: ,// how to update correctly
+      picture_url: , 
+      description: ,
+      title:  
+    }).then((response) => {
+      console.log('Workouts updated successfully');
+      // setState()
+    }).catch((error) => {
+      console.log('There is an error updating workouts: ', error);
+    })
+
+    axios.post('http://localhost:8080/api/wokrouts/remove', {
+      workout_id: 
+    }).then((response) => {
+      console.log('Workout removed successfully');
+      // setState()
+    }).catch((error) => {
+      console.log('There is an removing workout ', error);
+    });
+
+
+
   }, [])
 
   return { state }
