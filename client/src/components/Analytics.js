fab87a0019e156582462de0d5a95b4d88db23bac
Wendina <wendina.teng@gmail.com>
2023-05-22 03:22:19 -0400

    create mock data for analytics and import line from recharts

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -1,11 +1,43 @@
 import React from "react";
+import { LineChart, Line } from 'recharts';
 
 export function Analytics({numWorkouts, topFiveExercises, exerciseProgress, workoutsByMonth}) {
-
-  console.log('COUNT: ', numWorkouts);
-  console.log('TOP FIVE: ', topFiveExercises);
+  // console.log('COUNT: ', numWorkouts);
+  // console.log('TOP FIVE: ', topFiveExercises);
+  // console.log('WORKOUTS BY MONTH: ', workoutsByMonth);
   console.log('EXERCISE PROGRESS: ', exerciseProgress);
-  console.log('WORKOUTS BY MONTH: ', workoutsByMonth);
+
+  const data = [
+    {
+      date: "2023-05-02T04:00:00.000Z",
+      exercise: "Bentover Rows",
+      weight: 250
+    },
+    {
+      date: "2023-05-02T04:00:00.000Z",
+      exercise: "Bench Press",
+      weight: 200
+    },
+    {
+      date: "2023-05-03T04:00:00.000Z",
+      exercise: "Bentover Rows",
+      weight: 200
+    },
+    {
+      date: "2023-05-04T04:00:00.000Z",
+      exercise: "Bentover Rows",
+      weight: 250
+    },
+    {
+      date: "2023-05-05T04:00:00.000Z",
+      exercise: "Bentover Rows",
+      weight: 300
+    }
+  ]
+
+  // const renderLineChart = (
+
+  // );
 
   return(
     <p>Hi from Analytics</p>
