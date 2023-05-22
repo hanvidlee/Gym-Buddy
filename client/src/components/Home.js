95e744ec231324282900b1f7078b0151690ac25a
nelsonchengg <nelsoncheng103@gmail.com>
2023-05-22 03:16:05 -0400

    style homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,76 +1,75 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import moment from 'moment';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-import './Home.scss';
+import './Home.scss'
 
 export default function Home({ workouts }) {
   return (
-    <div className="home-wrapper">
-      <>
-        <Link to="/calendar">
-          <Button
-            variant="contained"
+    <div class="home-wrapper">
+      <Link to="/calendar">
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: 'green',
+            '&:hover': { backgroundColor: 'green' },
+            margin: '1em',
+          }}
+        >
+          Add Workout
+        </Button>
+      </Link>
+      {workouts.map((workout) => {
+        return (
+          <Card
+            key={`workout-${workout.id}`}
+            elevation={6}
             sx={{
-              backgroundColor: 'green',
-              '&:hover': { backgroundColor: 'green' },
-              margin: '1em',
+              paddingBottom: '1em',
+              maxWidth: '425px',
+              margin: '0 auto',
+              marginBottom: '1em',
+              backgroundColor: "rgba(0, 0, 0, 0.9)",
+              color: "white"
             }}
           >
-            Add Workout
-          </Button>
-        </Link>
-        {workouts.map((workout) => {
-          return (
-            <Card
-              key={`workout-${workout.id}`}
-              elevation={6}
-              sx={{
-                paddingBottom: '1em',
-                maxWidth: '425px',
-                margin: '0 auto',
-                marginBottom: '1em',
-              }}
-            >
-              <CardContent>
-                <Typography
-                  variant="h6"
-                  component="div"
-                  sx={{ textAlign: 'left' }}
-                >
-                  {moment(workout.workout_date).format('MMMM Do YYYY')}
-                </Typography>
-                <CardHeader
-                  subheader={workout.title}
-                  sx={{
-                    padding: '0',
-                    textAlign: 'left',
-                    paddingBottom: '0.2em',
-                  }}
+            <CardContent>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ textAlign: 'left' }}
+              >
+                {moment(workout.workout_date).format('MMMM Do YYYY')}
+              </Typography>
+              <Typography sx={{
+                padding: '0',
+                textAlign: 'left',
+                paddingBottom: '0.2em',
+                color: "white"
+              }}>
+                {workout.title}
+              </Typography>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 126, height: 126, marginRight: '1em' }}
+                  image={workout.picture_url}
                 />
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <CardMedia
-                    component="img"
-                    sx={{ width: 126, height: 126, marginRight: '1em' }}
-                    image={workout.picture_url}
-                  />
-                  <Typography variant="body2" color="text.secondary">
-                    {workout.description}
-                  </Typography>
-                </div>
-              </CardContent>
-              <Link to={`/log/show/${workout.id}`}>
-                <Button variant="contained">View Details</Button>
-              </Link>
-            </Card>
-          );
-        })}
-      </>
+                <Typography variant="body2" color="white">
+                  {workout.description}
+                </Typography>
+              </div>
+            </CardContent>
+            <Link to={`/log/show/${workout.id}`}>
+              <Button variant="contained">View Details</Button>
+            </Link>
+          </Card>
+        );
+      })}
     </div>
   );
 }
