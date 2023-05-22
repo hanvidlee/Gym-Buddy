8539f103e6ebcfbf7d13657d4ad6d1f7d1ce958c
hanvidlee <hanvidlee@gmail.com>
2023-05-21 20:19:00 -0400

    add css to the form

--- client/src/components/FormTest.js
+++ client/src/components/FormTest.js
@@ -56,8 +56,8 @@ export default function FormTest(props) {
   };
 
   return (
-    <Card elevation={6} sx={{ marginBottom: "1em", maxWidth: "425px", margin: "0 auto" }}>
-      <CardContent>    <form onSubmit={onSubmit}>
+    <Card elevation={6} sx={{ paddingBottom: "1em", maxWidth: "425px", margin: "0 auto" }}>
+      <form onSubmit={onSubmit}>
         <Typography variant="h6">Workout Form</Typography>
         {selectedImage && (
           <div>
@@ -67,172 +67,218 @@ export default function FormTest(props) {
               src={URL.createObjectURL(selectedImage)}
               value={selectedImage}
             />
-            <br />
             <Button onClick={() => setSelectedImage(null)}>Remove</Button>
           </div>
         )}
 
-        <input
-          type="file"
-          name="myImage"
-          onChange={(event) => {
-            console.log(event.target.files[0]);
-            setSelectedImage(event.target.files[0]);
-          }}
-        />
-        <TextField
-          required
-          label="Workout Title"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)
+        <CardContent sx={{ paddingBottom: "0px" }}>
+          <TextField
+            label="Title"
+            required
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            defaultValue="Title"
+            InputProps={{
+              endAdornment: <InputAdornment position="start"></InputAdornment>
+            }}
+            InputLabelProps={{
+              shrink: true
+            }}
+            sx={{
+              '& .MuiInputBase-input': {
+                fontSize: '13px',
+                padding: '4px 3px',
+              },
+            }}
+          />
+        </CardContent>
+        <CardContent>
+          <input
+            type="file"
+            name="myImage"
+            onChange={(event) => {
+              console.log(event.target.files[0]);
+              setSelectedImage(event.target.files[0]);
+            }}
+          />
+        </CardContent>
+        <CardContent sx={{ padding: "0px" }}>
+          <TextField
+            id="outlined-multiline-static"
+            multiline
+            label="Description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            InputProps={{
+              endAdornment: <InputAdornment position="start"></InputAdornment>
+            }}
+            InputLabelProps={{
+              shrink: true
+            }}
+            sx={{
+              width: "390px",
+              '& .MuiOutlinedInput-root': {
+                padding: '4px 10px'
+              },
+              '& .MuiInputBase-input': {
+                fontSize: '13px',
+                padding: '4px 3px',
+              },
+            }}
+          />
+        </CardContent>
+        <CardContent>
+          <TableContainer component={Paper}>
+            <Table size="small" aria-label="a dense table">
+              <TableBody>
+                {exerciseSets.map((es, index) => {
+                  return (
+                    <TableRow
+                      key={es.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell>
+                        <Autocomplete
+                          key={`combo-box-demo-${index}`}
+                          className="form-dropdown"
+                          disablePortal
+                          id="combo-box-demo"
+                          options={exercises}
+                          align="right"
+                          onChange={(event, newValue) => {
+                            setValue(newValue);
+                            const updatedExerciseSets = [...exerciseSets];
+                            updatedExerciseSets[index] = {
+                              ...updatedExerciseSets[index],
+                              exercise: newValue
+                            };
+                            setExerciseSets(updatedExerciseSets);
+                          }}
+                          value={es.exercise || ""}
+                          inputValue={es.exercise}
+                          onInputChange={(event, newInputValue) => {
+                            setInputValue(newInputValue);
+                          }}
+                          renderInput={(params) => (
+                            <TextField
+                              {...params}
+                              required
+                              label="Exercises"
+                              InputLabelProps={{
+                                shrink: true
+                              }}
+                              sx={{
+                                maxWidth: "auto",
+                                '& .MuiInputBase-input': {
+                                  fontSize: '13px',
+                                  padding: '4px 3px',
+                                },
+                              }}
+                            />
+                          )}
+                        />
+                      </TableCell>
+                      <TableCell align="right" sx={{ maxWidth: '55px', padding: '4px 2px' }}>
+                        <TextField
+                          required
+                          label="lbs"
+                          value={es.weight}
+                          InputProps={{
+                            endAdornment: <InputAdornment position="start"></InputAdornment>
+                          }}
+                          InputLabelProps={{
+                            shrink: true
+                          }}
+                          sx={{
+                            '& .MuiInputBase-input': {
+                              fontSize: '13px',
+                              padding: '4px 3px',
+                            },
+                          }}
+                          onChange={(event) => {
+                            const updatedExerciseSets = [...exerciseSets];
+                            updatedExerciseSets[index] = {
+                              ...updatedExerciseSets[index],
+                              weight: event.target.value
+                            };
+                            setExerciseSets(updatedExerciseSets);
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell align="right" sx={{ maxWidth: '55px', padding: '4px 2px' }}>
+                        <TextField
+                          required
+                          label="x"
+                          value={es.quantity}
+                          InputLabelProps={{
+                            shrink: true
+                          }}
+                          sx={{
+                            '& .MuiInputBase-input': {
+                              fontSize: '13px',
+                              padding: '4px 3px',
+                            },
+                          }}
+                          onChange={(event) => {
+                            const updatedExerciseSets = [...exerciseSets];
+                            updatedExerciseSets[index] = {
+                              ...updatedExerciseSets[index],
+                              quantity: event.target.value
+                            };
+                            setExerciseSets(updatedExerciseSets);
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell align="right" sx={{ maxWidth: '55px', padding: '4px 2px' }}>
+                        <TextField
+                          required
+                          label="reps"
+                          value={es.reps}
+                          InputLabelProps={{
+                            shrink: true
+                          }}
+                          sx={{
+                            '& .MuiInputBase-input': {
+                              fontSize: '13px',
+                              padding: '4px 3px',
+                            },
+                          }}
+                          onChange={(event) => {
+                            const updatedExerciseSets = [...exerciseSets];
+                            updatedExerciseSets[index] = {
+                              ...updatedExerciseSets[index],
+                              reps: event.target.value
+                            };
+                            setExerciseSets(updatedExerciseSets);
+                          }}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </CardContent>
+      <Button type="submit" variant="contained" sx={{ backgroundColor: "green", "&:hover": { backgroundColor: "green" } }}>Submit</Button>
+      <Button type="button" variant="contained" sx={{ margin: "0 1em" }} onClick={() => {
+        setExerciseSets([
+          ...exerciseSets,
+          {
+            exercise: "",
+            reps: 0,
+            quantity: 0,
+            weight: 0
           }
-          defaultValue=""
-        />
-        <TextField
-          label="Picture"
-          value={selectedImage}
-          onChange={(event) => setWorkout({ ...workout, picture: event.target.value })}
-        />
-        <TextField
-          label="Description"
-          value={description}
-          onChange={(event) => setDescription({ ...description, description: event.target.value })}
-        />
-        <TableContainer component={Paper}>
-          <Table size="small" aria-label="a dense table">
-            <TableBody>
-              {exerciseSets.map((es, index) => {
-                return (
-                  <TableRow >
-                    <TableCell>
-                      <Autocomplete
-                        key={`combo-box-demo-${index}`}
-                        className="form-dropdown"
-                        disablePortal
-                        id="combo-box-demo"
-                        options={exercises}
-                        align="right"
-                        sx={{
-                          '& .MuiInputBase-input': {
-                            fontSize: '13px',
-                            padding: '4px 3px',
-                          },
-                        }}
-                        onChange={(event, newValue) => {
-                          setValue(newValue);
-                          const updatedExerciseSets = [...exerciseSets];
-                          updatedExerciseSets[index] = {
-                            ...updatedExerciseSets[index],
-                            exercise: newValue
-                          };
-                          setExerciseSets(updatedExerciseSets);
-                        }}
-                        value={es.exercise || ""}
-                        inputValue={es.exercise}
-                        onInputChange={(event, newInputValue) => {
-                          setInputValue(newInputValue);
-                        }}
-                        renderInput={(params) => (
-                          <TextField
-                            {...params}
-                            required
-                            label="Exercises"
-                          />
-                        )}
-                      />
-                    </TableCell>
-                    <TableCell align="right" sx={{ maxWidth: '55px', padding: '4px 2px' }}>
-                      <TextField
-                        required
-                        label="lbs"
-                        InputProps={{
-                          endAdornment: <InputAdornment position="start"></InputAdornment>
-                        }}
-                        value={es.weight}
-                        sx={{
-                          '& .MuiInputBase-input': {
-                            fontSize: '13px',
-                            padding: '4px 3px',
-                          },
-                        }}
-                        onChange={(event) => {
-                          const updatedExerciseSets = [...exerciseSets];
-                          updatedExerciseSets[index] = {
-                            ...updatedExerciseSets[index],
-                            weight: event.target.value
-                          };
-                          setExerciseSets(updatedExerciseSets);
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell align="right" sx={{ maxWidth: '55px', padding: '4px 2px' }}>
-                      <TextField
-                        required
-                        label="x"
-                        value={es.quantity}
-                        sx={{
-                          '& .MuiInputBase-input': {
-                            fontSize: '13px',
-                            padding: '4px 3px',
-                          },
-                        }}
-                        onChange={(event) => {
-                          const updatedExerciseSets = [...exerciseSets];
-                          updatedExerciseSets[index] = {
-                            ...updatedExerciseSets[index],
-                            quantity: event.target.value
-                          };
-                          setExerciseSets(updatedExerciseSets);
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell align="right" sx={{ maxWidth: '55px', padding: '4px 2px' }}>
-                      <TextField
-                        required
-                        label="reps"
-                        value={es.reps}
-                        sx={{
-                          '& .MuiInputBase-input': {
-                            fontSize: '13px',
-                            padding: '4px 3px',
-                          },
-                        }}
-                        onChange={(event) => {
-                          const updatedExerciseSets = [...exerciseSets];
-                          updatedExerciseSets[index] = {
-                            ...updatedExerciseSets[index],
-                            reps: event.target.value
-                          };
-                          setExerciseSets(updatedExerciseSets);
-                        }}
-                      />
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </form>
-      </CardContent>
-        <Button type="submit" variant="contained" sx={{ backgroundColor: 'green' }}>Submit</Button>
-        <Button type="button" variant="contained" sx={{ margin: "0 1em" }} onClick={() => {
-          setExerciseSets([
-            ...exerciseSets,
-            {
-              exercise: "",
-              reps: 0,
-              quantity: 0,
-              weight: 0
-            }
-          ]);
+        ]);
 
-        }}>Add row</Button>
-        <Button type="button" variant="contained" sx={{ backgroundColor: 'red' }} onClick={() => {
-          const updatedExerciseSets = [...exerciseSets];
-          updatedExerciseSets.pop();
-          setExerciseSets(updatedExerciseSets);
-        }}> Delete row </Button>
+      }}>Add row</Button>
+      <Button type="button" variant="contained" sx={{ backgroundColor: "red", "&:hover": { backgroundColor: "red" } }} onClick={() => {
+        const updatedExerciseSets = [...exerciseSets];
+        updatedExerciseSets.pop();
+        setExerciseSets(updatedExerciseSets);
+      }}> Delete row </Button>
+      </form>
     </Card>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -40,6 +40,6 @@ export default function Home({
           </Card>
         );
       })}
-    </div>
+    </>
   );
 }
