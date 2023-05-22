50e1350fc6ed58a470e5ef036ea71e6cdfc967f4
Wendina <wendina.teng@gmail.com>
2023-05-21 22:02:17 -0400

    wrap a <div> around the fragment

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,39 +7,70 @@ import moment from 'moment';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import './Home.scss';
 
-export default function Home({
-  workouts
-}) {
+export default function Home({ workouts }) {
   return (
-    <>
-      <Link to="/calendar"><Button variant="contained" sx={{ backgroundColor: "green", "&:hover": { backgroundColor: "green" }, margin: "1em" }}>Add Workout</Button></Link>
-      {workouts.map((workout) => {
-        return (
-          <Card key={`workout-${workout.id}`} elevation={6} sx={{ paddingBottom: "1em", maxWidth: "425px", margin: "0 auto", marginBottom: "1em" }}>
-            <CardContent>
-              <Typography variant="h6" component="div" sx={{ textAlign: "left" }} >
-                {moment(workout.workout_date).format("MMMM Do YYYY")}
-              </Typography>
-              <CardHeader
-                subheader={workout.title}
-                sx={{ padding: "0", textAlign: "left", paddingBottom: "0.2em" }}
-              />
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <CardMedia
-                  component="img"
-                  sx={{ width: 126, height: 126, marginRight: "1em" }}
-                  image={workout.picture_url}
-                />
-                <Typography variant="body2" color="text.secondary">
-                  {workout.description}
+    <div className="home-wrapper">
+      <>
+        <Link to="/calendar">
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: 'green',
+              '&:hover': { backgroundColor: 'green' },
+              margin: '1em',
+            }}
+          >
+            Add Workout
+          </Button>
+        </Link>
+        {workouts.map((workout) => {
+          return (
+            <Card
+              key={`workout-${workout.id}`}
+              elevation={6}
+              sx={{
+                paddingBottom: '1em',
+                maxWidth: '425px',
+                margin: '0 auto',
+                marginBottom: '1em',
+              }}
+            >
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ textAlign: 'left' }}
+                >
+                  {moment(workout.workout_date).format('MMMM Do YYYY')}
                 </Typography>
-              </div>
-            </CardContent>
-            <Link to={`/log/show/${workout.id}`}><Button variant="contained">View Details</Button></Link>
-          </Card>
-        );
-      })}
-    </>
+                <CardHeader
+                  subheader={workout.title}
+                  sx={{
+                    padding: '0',
+                    textAlign: 'left',
+                    paddingBottom: '0.2em',
+                  }}
+                />
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <CardMedia
+                    component="img"
+                    sx={{ width: 126, height: 126, marginRight: '1em' }}
+                    image={workout.picture_url}
+                  />
+                  <Typography variant="body2" color="text.secondary">
+                    {workout.description}
+                  </Typography>
+                </div>
+              </CardContent>
+              <Link to={`/log/show/${workout.id}`}>
+                <Button variant="contained">View Details</Button>
+              </Link>
+            </Card>
+          );
+        })}
+      </>
+    </div>
   );
 }
