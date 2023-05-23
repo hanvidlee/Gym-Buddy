d05b7cd8be37b688b8a459d777bfd1f9680c4a35
Wendina <wendina.teng@gmail.com>
2023-05-23 14:32:56 -0400

    change the name of props being passed to numOfWorkouts

--- client/src/components/Analytics/Analytics.js
+++ client/src/components/Analytics/Analytics.js
@@ -2,16 +2,14 @@ import React from "react";
 import ExerciseProgress from "./ExerciseProgress";
 import TopFiveExercises from "./TopFiveExercises";
 import WorkoutsByMonth from "./WorkoutsByMonth";
+import NumOfWorkouts from "./NumOfWorkouts";
 
 export default function Analytics({numWorkouts, topFiveExercises, exerciseProgress, workoutsByMonth}) {
-  // console.log('COUNT: ', numWorkouts);
-  // console.log('TOP FIVE: ', topFiveExercises);
-  console.log('WORKOUTS BY MONTH: ', workoutsByMonth);
-  // console.log('EXERCISE PROGRESS: ', exerciseProgress);
 
   return(
     <>
       <h1>Exercise Analytics</h1>
+      <NumOfWorkouts count={numWorkouts} />
       <WorkoutsByMonth workouts={workoutsByMonth} />
       <TopFiveExercises exercises={topFiveExercises} />
       <ExerciseProgress exercises={exerciseProgress} />
