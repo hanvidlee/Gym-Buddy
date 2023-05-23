e2a9927bcf47c84ae15afec88681c771a2e5cda5
Wendina <wendina.teng@gmail.com>
2023-05-23 07:30:57 -0400

    add scss to top 5 exercises

--- client/src/components/Analytics/TopFiveExercises.js
+++ client/src/components/Analytics/TopFiveExercises.js
@@ -6,28 +6,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import './TopFiveExercises.scss';
 
 export default function TopFiveExercises({ exercises }) {
   console.log('TOP FIVE EXERCISES: ', exercises);
 
   // style={{ backgroundColor: 'white', padding: '20 px' }}
   return (
-    <div className="table">
+    <div className="Table">
         <h3>Top 5 Exercises</h3>
 
-      <TableContainer component={Paper}>
+      <TableContainer component={Paper} style={{ boxShadow: '0px 13px 20px 0px #80808029 ' }}>
         <Table sx={{ maxWidth: 350 }} aria-label="simple table">
           <TableHead>
             <TableRow>
